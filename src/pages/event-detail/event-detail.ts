861311f3646ddd2cb3d3c43c7b297bdaa7fd5298
SubroBlack <dahalsubodh8@gmail.com>
2019-03-05 16:25:03 +0200

    Opens event detail page when clicked on the card

---
+++ src/pages/event-detail/event-detail.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import { EventProvider } from '../../providers/event/event';
+
+/**
+ * Generated class for the EventDetailPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-event-detail',
+  templateUrl: 'event-detail.html',
+})
+export class EventDetailPage {
+  event: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public events: EventProvider) {
+    this.event = navParams.get('event');
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad EventDetailPage');
+  }
+
+}
